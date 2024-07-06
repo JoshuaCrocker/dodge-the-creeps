bf771a202602a4f38c695fbd1428e80dbecc55da
Joshua Crocker <222282+JoshuaCrocker@users.noreply.github.com>
2024-07-06 14:24:44 +0100

    Add HUD

--- main.gd
+++ main.gd
@@ -12,15 +12,17 @@ func _ready():
 func _process(delta):
 	pass
 
-
 func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
 
 func new_game():
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
 
 
 func _on_mob_timer_timeout():
@@ -51,6 +53,7 @@ func _on_mob_timer_timeout():
 
 func _on_score_timer_timeout():
 	score += 1
+	$HUD.update_score(score)
 
 
 func _on_start_timer_timeout():
